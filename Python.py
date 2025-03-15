2c0c6d4c556d0db071cc347c0513dc4247aeb35b
Abhishek Tiwari <66767924+1Abhishek-programmer@users.noreply.github.com>
2025-03-16 01:55:08 +0530

    Add files via upload

---
+++ Python.py
@@ -0,0 +1,29 @@
+#Assigment 1: 
+#Task 1: Perform Basic Mathematical Operations
+#Problem Statement: Write a Python program that does the following:
+#1.  Takes two numbers as input from the user.
+A =input('Enter the first number')
+print(A)
+B =input('Enter the second number')
+print(B)
+
+#2.  Performs the basic mathematical operations on these two numbers:
+# Addition, Subtraction, Multiplication, Division
+A =5
+B =10
+res =A+B
+print("Addition:",res)
+res =A-B
+print("Substraction:",res)
+res =A*B
+print("Multiplication:",res)
+res =A/B
+print("Division :",res)
+#Task 2: Create a Personalized Greeting
+#Problem Statement: Write a Python program that:
+#1.  Takes a user's first name and last name as input.
+A =input("input your first name:")
+B=input("input your last name:")
+
+#2.  Prints a personalized greeting message using the full name.
+print("Hello Abhishek Tiwari!Welcome to the python program",)
