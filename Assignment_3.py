6bfe648f3cf69dc5884e9a1aef14ad3ca3b4c7ef
Abhishek Tiwari <tiwariisabhishek1@gmail.com>
2025-04-07 00:40:07 +0530

    Assignment 3

--- Assignment_3.py
+++ Assignment_3.py
@@ -2,12 +2,29 @@
 # Task 1: Calculate Factorial Using a Function 
 # Problem Statement: Write a Python program that:
 # 1.   Defines a function named factorial that takes a number as an argument and calculates its factorial using a loop or recursion.
-def facatorial(x):
-    num = 1;
-    for i in range(x):
-        num*=i
-    print(num)
+def factorial(n):
+    if n<2:
+        return 1
+    else:
+        return (n * factorial(n-1))
+result = factorial(5)
+print('Factorial of 5 is:',result)
+  
 
-facatorial(5)
-
-    
+# Task 2: Using the Math Module for Calculations
+ 
+# Problem Statement: Write a Python program that:
+# 1.   Asks the user for a number as input.
+# 2.   Uses the math module to calculate the:
+# o   Square root of the number
+# o   Natural logarithm (log base e) of the number
+# o   Sine of the number (in radians)
+# 3.   Displays the calculated results.
+import math
+a =int(input("Enter a number:"))
+sqrt_a = math.sqrt(a)
+print("Square root:",sqrt_a)
+log_a =math.log(a)
+print("Logarithm:",log_a)
+sin_a =math.sin(a)
+print("Sin:",sin_a)
